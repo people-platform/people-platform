f79900417e49d7af2dc48668dca9816198498d0e
zhaoyunxing <zhaoyunxing@apache.org>
2023-06-06 22:45:10 +0800

    init

---
+++ src/main/java/org/people/platform/PeoplePlatformApplication.java
@@ -0,0 +1,13 @@
+package org.people.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeoplePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/people/platform/PeoplePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.people.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
